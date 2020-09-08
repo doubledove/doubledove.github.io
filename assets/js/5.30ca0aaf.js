37f383dc7c13e79b0029be5675e81da0db634562
hujun <hujun@aiqinhaigou.com>
2020-09-08 11:45:44 +0800

    deploy

---
+++ assets/js/5.30ca0aaf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{353:function(t,e,n){"use strict";n.r(e);var s=n(42),l=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"cpt-hello"},[this._v("咕咕咕")])}),[],!1,null,null,null);e.default=l.exports}}]);
